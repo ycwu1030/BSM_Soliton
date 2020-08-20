600c8a15f4461e1843378bb7f3136128cb86f049
Yongcheng Wu <yc_wu1030@126.com>
2020-08-20 12:11:56 -0400

    test(CM Monopole)
    Update the test file for CM monopole

--- test/CMMonopole_test.cpp
+++ test/CMMonopole_test.cpp
@@ -7,9 +7,14 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     CMMonopoleSolver solver;
-    cout<<"g: "<<solver.Getgweak()<<endl;
-    VD left = {0,1,0,-0.21*solver.Getgweak()};
-    VD right = {1,0,0.25*solver.Getgweak(),0};
+    double g=solver.Getgweak();
+    double rho0=solver.GetVEV();
+    double mw=solver.GetMW();
+    // cout<<"g: "<<solver.Getgweak()<<endl;
+    double A0=mw/2.0;
+
+    VD left = {0,1,0,-A0/rho0};
+    VD right = {1,0,A0/rho0,0};
 
     solver.SetBoundary(left,right);
     solver.SetMeshPoints(200);
@@ -23,6 +28,10 @@ int main(int argc, char const *argv[])
     good = solver.Solve(X,Y);
     solver.DumpSolution("CMMonopole_sol_ms10.dat");
 
+    solver.SetMHL(125.0);
+    good = solver.Solve(X,Y);
+    solver.DumpSolution("CMMonopole_sol_ms125.dat");
+
     solver.SetMHL(1000.0);
     good = solver.Solve(X,Y);
     solver.DumpSolution("CMMonopole_sol_ms1000.dat");
@@ -32,5 +41,30 @@ int main(int argc, char const *argv[])
     solver.SetXRange(0.5,20);
     good = solver.Solve(X,Y);
     solver.DumpSolution("CMMonopole_sol_ms10000.dat");
+
+    VD left0 = {0,1,0,0};
+    VD right0 = {1,0,A0/rho0,0};
+
+    solver.SetBoundary(left0,right0);
+    solver.SetMeshPoints(200);
+    solver.SetXRange(0.5,50);
+
+    solver.SetMHL(10.0);
+    good = solver.Solve(X,Y);
+    solver.DumpSolution("CMMonopole_sol_ms10_b00.dat");
+
+    solver.SetMHL(125.0);
+    good = solver.Solve(X,Y);
+    solver.DumpSolution("CMMonopole_sol_ms125_b00.dat");
+
+    solver.SetMHL(1000.0);
+    good = solver.Solve(X,Y);
+    solver.DumpSolution("CMMonopole_sol_ms1000_b00.dat");
+
+    solver.SetMHL(10000.0);
+    solver.SetMeshPoints(500);
+    solver.SetXRange(0.5,20);
+    good = solver.Solve(X,Y);
+    solver.DumpSolution("CMMonopole_sol_ms10000_b00.dat");
     return 0;
 }
