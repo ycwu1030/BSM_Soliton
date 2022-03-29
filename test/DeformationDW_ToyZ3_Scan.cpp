cfe906b08df43a5d72c23d19460496dcd105610b
Yongcheng Wu <yc_wu1030@126.com>
2022-03-29 16:13:52 +0800

    update z3 scan

---
+++ test/DeformationDW_ToyZ3_Scan.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include "PathDeformation.h"
+#include "ToyZ3.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    ToyZ3 model;
+    model.Set_Potential_Parameters(0.5);
+    // model.PrintParameters();
+    // model.PrintLocalMinima();
+
+    VnD vtol = [&](VD field, double scale) { return model.Vtotal(field, scale); };
+    dVnD dvtol = [&](VD field, double scale) { return model.dVtotal(field, scale); };
+
+    ofstream out("ToyZ3_Scan.dat");
+    out << "r tension width" << endl;
+    for (int rid = -30; rid <= 40; rid += 1) {
+        double r = pow(10, rid / 10.0);
+        model.Set_Potential_Parameters(r);
+        VVD pts_init;
+        VD left = model.GetLocalMinima(0);
+        VD right = model.GetLocalMinima(1);
+        pts_init.push_back(left);
+        pts_init.push_back(right);
+        KinknD sol = fullKink(pts_init, vtol, dvtol);
+        char fname[200];
+        sprintf(fname, "ToyZ3_Kink/ToyZ3_%d.dat", rid);
+        model.DumpFullSolution(sol.R, sol.Phi, sol.dPhi_1D, fname);
+        out << r << " " << model.GetTotalEnergy(sol.R, sol.Phi, sol.dPhi_1D) << " "
+            << model.GetWallWidth(sol.R, sol.Phi, sol.dPhi_1D) << endl;
+    }
+    double r = 3.0 / 4.0;
+    model.Set_Potential_Parameters(r);
+    VVD pts_init;
+    VD left = model.GetLocalMinima(0);
+    VD right = model.GetLocalMinima(1);
+    pts_init.push_back(left);
+    pts_init.push_back(right);
+    KinknD sol = fullKink(pts_init, vtol, dvtol);
+    char fname[200];
+    sprintf(fname, "ToyZ3_Kink/ToyZ3_3over4.dat");
+    model.DumpFullSolution(sol.R, sol.Phi, sol.dPhi_1D, fname);
+    return 0;
+}
